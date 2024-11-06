4560aa6c9a5f7b257fe71cdac5d88db6ce6e1551
Yead <yead19@gmail.com>
2024-11-06 18:21:28 +0600

    responsive done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer bg-white text-base-content p-10">
+        <div className='bg-white md:items-center md:justify-center'>
+            <div className='md:text-center p-2'>
+                <h1 className='font-bold text-4xl pt-16'>Gadget Heaven</h1>
+                <p className='text-base text-gray-500 leading-4 mt-3 mb-7 '>Leading the way in cutting-edge technology and innovation.</p>
+
+                <p className='text-xs'>Copyright © {new Date().getFullYear()} - All right reserved by yead.</p>
+
+            </div>
+            <div className='border border-base-200 mt-2'></div>
+            <footer className="footer bg-white text-base-content p-10 md:items-center md:justify-around mb-0">
                 <nav>
                     <h6 className="footer-title">Services</h6>
                     <a className="link link-hover">Branding</a>
@@ -25,7 +33,8 @@ const Footer = () => {
                     <a className="link link-hover">Cookie policy</a>
                 </nav>
             </footer>
-            
+
+
         </div>
     );
 };
